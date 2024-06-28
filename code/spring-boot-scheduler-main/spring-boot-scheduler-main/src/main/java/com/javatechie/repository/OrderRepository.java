7d6aa5b5e0dc2700267b91d226a9616f50a5c5b1
Yellaiah_Mekala <60250665+YellaiahMekala@users.noreply.github.com>
2024-06-28 12:00:20 +0530

    Delete code/spring-boot-scheduler-main directory

--- code/spring-boot-scheduler-main/spring-boot-scheduler-main/src/main/java/com/javatechie/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javatechie.repository;
-
-import com.javatechie.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
